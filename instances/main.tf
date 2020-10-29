596592fa0bd12c1e6a5bf3c20f8dcf07dc673649
Torben <torben.mannhart@gmail.com>
2020-10-29 14:18:11 +0100

    initial commit

---
+++ instances/main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a068806d7fab7873e62187d1143596acbe2427cead7ca0046d5cdb380c6b88b
+size 1662
---
+++ instances/variables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70ca0f25eb933193c047c6403ab8ff521d5e822e4f9f60d05184ae85b00826f3
+size 370
